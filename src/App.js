5d6fcd2e3bbe7cdb001d72578f61590fd4d4b361
github username <krishel.lasam@gmail.com>
2017-06-22 02:43:39 -0400

    added router and style app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Compare from './components/Compare.js'
+
+const UserProducts = () => (
+  <div>
+    <h2>My Products</h2>
+  </div>
+)
+
+const UserIngredients = () => (
+  <div>
+    <h2>My Ingredients</h2>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="text">Luminance</h1>
+        <Router>
+          <div class="navbar">
+            <div id="links">
+              <Link to="/">Compare</Link> 
+              <Link to="/users/:userId/products">My Products</Link>
+              <Link to="/users/:userId/ingredients">My Ingredients</Link>
+            </div>
+            <hr/>
+              <Route exact path="/" component={Compare}/>
+              <Route path="/users/:userId/products" component={UserProducts}/>
+              <Route path="/users/:userId/ingredients" component={UserIngredients}/>
+          </div>
+        </Router>
       </div>
     );
   }
