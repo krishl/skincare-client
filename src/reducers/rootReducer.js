bfc8792ea0fdbd62f5a3ec837256843febf2d024
github username <krishel.lasam@gmail.com>
2017-07-04 01:24:55 -0400

    import ingredientReducer to rootReducer and combine

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';  
 import products from './productReducer';
+import ingredients from './ingredientReducer';
 
 const rootReducer = combineReducers({  
-  products
+  products,
+  ingredients
 })
 
 export default rootReducer;  
